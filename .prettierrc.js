29cb79526ed05ff083edb4ef974abe776112415d
Olivier Louvignes <olivier@mgcrea.io>
2019-09-03 12:46:31 +0200

    chore(prettier): move to .js file

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// {
+//   "printWidth": 120,
+//   "semi": true,
+//   "singleQuote": true,
+//   "bracketSpacing": false,
+//   "arrowParens": "avoid"
+// }
+
+module.exports = {
+  printWidth: 120,
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  semi: true,
+  // trailingComma: 'all',
+};
