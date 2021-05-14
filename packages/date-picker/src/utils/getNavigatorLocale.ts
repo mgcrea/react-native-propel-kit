6768b6c6faec4ae7b21e6ed874004286cbff42de
Olivier Louvignes <olivier@mgcrea.io>
2021-05-14 16:42:24 +0200

    fix(android): fix android error

--- packages/date-picker/src/utils/getNavigatorLocale.ts
+++ packages/date-picker/src/utils/getNavigatorLocale.ts
@@ -1,9 +1,14 @@
 import {Platform, NativeModules} from 'react-native';
 
 export const getNavigatorLocale = () => {
-  return Platform.select<string>({
-    ios: NativeModules.SettingsManager.settings.AppleLocale || NativeModules.SettingsManager.settings.AppleLanguages[0],
-    android: NativeModules.I18nManager.localeIdentifier,
-    default: 'en-US'
-  });
+  switch (Platform.OS) {
+    case 'ios':
+      return (
+        NativeModules.SettingsManager.settings.AppleLocale || NativeModules.SettingsManager.settings.AppleLanguages[0]
+      );
+    case 'android':
+      return NativeModules.I18nManager.localeIdentifier;
+    default:
+      return 'en-US';
+  }
 };
