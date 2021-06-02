70d94adbd7811161aa8f9229d603fcabf37a1d24
shibatanien <61811629+shibatanien@users.noreply.github.com>
2021-06-02 13:46:16 +0800

    Update localizedMonths.ts

--- packages/date-picker/src/utils/localizedMonths.ts
+++ packages/date-picker/src/utils/localizedMonths.ts
@@ -13,6 +13,20 @@ export const localizedMonths: {[s: string]: Array<string>} = {
     'Novembre',
     'Décembre'
   ],
+  ja: [
+    '１月',
+    '２月',
+    '３月',
+    '４月',
+    '５月',
+    '６月',
+    '７月',
+    '８月',
+    '９月',
+    '１０月',
+    '１１月',
+    '１２月'
+  ],
   en: [
     'January',
     'February',
