ca19a1ec2661a45d131a552a064b1262ba04dfc6
Nguyen Anh Phuong <30553637+N-A-P@users.noreply.github.com>
2021-06-28 20:49:03 +0700

    add localized for vietnamese

--- packages/date-picker/src/utils/localizedMonths.ts
+++ packages/date-picker/src/utils/localizedMonths.ts
@@ -26,5 +26,19 @@ export const localizedMonths: {[s: string]: Array<string>} = {
     'October',
     'November',
     'December'
-  ]
+  ],
+   vi: [
+    'Tháng 1,
+    'Tháng 2',
+    'Tháng 3',
+    'Tháng 4',
+    'Tháng 5',
+    'Tháng 6',
+    'Tháng 7',
+    'Tháng 8',
+    'Tháng 9',
+    'Tháng 10',
+    'Tháng 11',
+    'Tháng 12',
+  ],
 };
